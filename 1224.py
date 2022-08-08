c1ce8db704cff1d67e21b0b5fe46ea2fd8645a06
TitanXxX <87604154+TitanXxX@users.noreply.github.com>
2022-08-08 14:32:41 +0400

    Add files via upload

---
+++ 1224.py
@@ -0,0 +1,10 @@
+import sys
+if(False):file=sys.stdin
+else:file=open("input.txt","r")
+n,m=list(map(int,file.read().split()))
+file.close()
+if((m==1) and (n!=1)):print(1)
+elif(n==1):print(0)
+else:print((min(n,m)-1)*2+int(m<n))
+
+
