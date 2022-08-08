388e2a6c41ac408c71b664fcbec365891cf309da
TitanXxX <87604154+TitanXxX@users.noreply.github.com>
2022-08-08 14:13:56 +0400

    Add files via upload

---
+++ 1607.py
@@ -0,0 +1,15 @@
+import sys
+if(False):file=sys.stdin
+else:file=open("input.txt","r")
+Petr,Petr_delta,Taks,Taks_delta=list(map(int,file.read().split()))
+file.close()
+if(Taks<=Petr):print(Petr)
+else:
+	while(True):
+		if(Petr+Petr_delta>=Taks):print(Taks);break
+		Petr+=Petr_delta
+		if(Taks-Taks_delta<=Petr):print(Petr);break
+		Taks-=Taks_delta
+
+
+
