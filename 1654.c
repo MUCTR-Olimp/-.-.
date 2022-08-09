72743be0938b0b5eaed335a5fdd97c959c6b277e
TitanXxX <87604154+TitanXxX@users.noreply.github.com>
2022-08-09 22:40:56 +0400

    Add files via upload

--- 1654.c
+++ 1654.c
@@ -4,10 +4,13 @@
 int main(){
 	char text[len];
 	unsigned int i=0,g=0;
-	FILE *file=fopen("input.txt","r");
-	fscanf(file,"%s",&text);
-	fclose(file);
-	//scanf("%s",&text);
+	if(1){
+		FILE *file=fopen("input.txt","r");
+		fscanf(file,"%s",&text);
+		fclose(file);
+	}else{
+		scanf("%s",&text);
+	}
 	while((i<len-1) && (text[i+1]!='\0')){
 		if(text[i]!='.'){
 			g=i+1;
@@ -17,14 +20,10 @@ int main(){
 				text[g]='.';
 				if(i!=0) i-=2;
 				while((text[i]=='.') && (i>0)) i--;
-				if(text[i]=='.'){
-					while((text[i]=='.') && (i<len-1) && (text[g+1]!='\0')) i++;
-				}
 			}
 		}
 		i++;
 	}
-	//printf("%s\n",text);
 	for (i=0;text[i]!='\0';i++) {
 		if(text[i]!='.') printf("%c",text[i]);
 	}
